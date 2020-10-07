9cc5236fcc6b3c68e238faa9fb42f2070a552557
Vladislav Kantariya <umorphine@gmail.com>
2020-10-08 01:25:12 +0400

    Added package slider settings

--- #src/js/slickSettings.js
+++ #src/js/slickSettings.js
@@ -4,5 +4,27 @@ $(document).ready(function(){
         dots: true,
         adaptiveHeight: true,
         slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+    });
+
+    $('.slider-package__slider').slick({
+        arrows: true,
+        infinite: true,
+        adaptiveHeight: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                dots: false
+              }
+            }
+        ],
     });
 });
