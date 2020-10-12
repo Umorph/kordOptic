6273cacfa212906c8a7ff8b44ee46f38b573f57b
Vladislav Kantariya <umorphine@gmail.com>
2020-10-12 15:50:21 +0400

    Added open/close logic for mobile nav

--- #src/js/modalNav.js
+++ #src/js/modalNav.js
@@ -5,3 +5,24 @@ aboutModalButton.addEventListener('click', function(evt) {
     evt.preventDefault();
     aboutModalWindow.classList.toggle('header__modal-about--opened');
 });
+
+let adaptiveNavWindow = document.querySelector('.modal-menu'),
+adaptiveNavOpenButton = document.querySelector('.header__burger-button'),
+adaptiveNavCloseButton = document.querySelector('.modal-menu__close-button');
+
+adaptiveNavOpenButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
+
+    if (adaptiveNavWindow.classList.contains('modal-menu--opened')) {
+        document.body.classList.add('lock');
+    } else {
+        document.body.classList.remove('lock');
+    }
+    
+    adaptiveNavWindow.classList.toggle('modal-menu--opened');
+});
+
+adaptiveNavCloseButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    adaptiveNavWindow.classList.toggle('modal-menu--opened');
+});
