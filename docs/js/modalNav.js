94f220d6245bad6dd859c81c18125767f5a2cc79
Vladislav <umorphine@gmail.com>
2020-12-16 04:52:20 +0400

    Деплой изменений на проду

--- docs/js/modalNav.js
+++ docs/js/modalNav.js
@@ -6,27 +6,6 @@ aboutModalButton.addEventListener('click', function(evt) {
     aboutModalWindow.classList.toggle('header__modal-about--opened');
 });
 
-let adaptiveNavWindow = document.querySelector('.modal-menu'),
-adaptiveNavOpenButton = document.querySelector('.header__burger-button'),
-adaptiveNavCloseButton = document.querySelector('.modal-menu__close-button');
-
-adaptiveNavOpenButton.addEventListener('click', function(evt) {
-    evt.preventDefault();
-
-    if (adaptiveNavWindow.classList.contains('modal-menu--opened')) {
-        document.body.classList.add('lock');
-    } else {
-        document.body.classList.remove('lock');
-    }
-    
-    adaptiveNavWindow.classList.toggle('modal-menu--opened');
-});
-
-adaptiveNavCloseButton.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    adaptiveNavWindow.classList.toggle('modal-menu--opened');
-});
-
 // online store modal
 
 let modalStoreWindow = document.querySelector('.modal-store'),
