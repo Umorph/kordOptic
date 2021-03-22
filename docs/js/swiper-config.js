d2b8894281df2859fe4bf5ffe3293ef2235bf409
Vladislav <umorphine@gmail.com>
2021-03-22 05:43:51 +0400

    Деплой изменений

--- docs/js/swiper-config.js
+++ docs/js/swiper-config.js
@@ -3,6 +3,7 @@ new Swiper('.merit-slider', {
   spaceBetween: 15,
   watchOverflow: true,
   loop: true,
+  grabCursor: true,
   autoplay: {
     delay: 1000,
     disableOnInteraction: false
@@ -19,3 +20,40 @@ new Swiper('.merit-slider', {
     }
   }
 });
+
+new Swiper('.chronology__mini-slider', {
+  slidesPerView: 'auto',
+  spaceBetween: 13,
+  watchOverflow: true,
+  loop: true,
+  slideToClickedSlide: true,
+  grabCursor: true,
+  thumbs: {
+    swiper: {
+      el: '.chronology__slider',
+      slidesPerView: 1,
+      touchRatio: 0,
+      simulateTouch: false,
+    }
+  },
+  navigation: {
+    prevEl: '.chronology__slider-control--prev',
+    nextEl: '.chronology__slider-control--next'
+  },
+  autoplay: {
+    delay: 15000,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    675: {
+      spaceBetween: 20,
+    },
+    1020: {
+      spaceBetween: 40,
+    },
+    1365: {
+      spaceBetween: 60,
+    }
+  }
+});
+
